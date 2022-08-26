860bac88a2b68c616173090fd6570051ca25ce49
bzhanane <95631502+bzhanane@users.noreply.github.com>
2022-08-26 17:30:21 +0200

    page home responsive
    fin de l'intégration de la page home  et responsive. Prochaine étape, faire le MultiSteps

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import logo from './assets/big_politips.png';
 import './App.css';
-import './components/WelcomeSteps'
+import './components/WelcomeSteps';
 import WelcomeSteps from './components/WelcomeSteps';
+import './styles/reset.css';
+import './styles/helpers.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-container">
     <img src={logo} className="App-logo" alt="logo"/>
     <WelcomeSteps />
-    <span></span>
+    <span className='bottom-bar'></span>
     </div>
   );
 }
--- src/components/WelcomeSteps.js
+++ src/components/WelcomeSteps.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+import "../styles/WelcomeSteps.css";
+import arrow from "../assets/arrow-right-solid.svg";
+
 
 export default class WelcomeSteps extends React.Component {
     render() {
         return (
-            <div>
-                <p></p>
+            <div className="text-welcome">
+                <h2>Bienvenue sur Politips !</h2>
+                <p>Pose un nouveau regard sur le monde en apprenant à le comprendre !</p>
+                <div className="bars-steps">
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+                <div className="img-arrow">
+                <img src={arrow} className="arrow" alt="arrow"/>
+                </div>
             </div>
         );
     }
