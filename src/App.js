3a88f278615c7913ebd5bd1774769e03c30d2728
Hawke008 <82276201+Hawke008@users.noreply.github.com>
2022-08-26 16:36:30 +0200

    Fix du fix du fix du conflit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logoReact from './logo.svg';
 import './css/reset.css'
 import './css/helpers.css'
 import './App.css';
+import WelcomeSteps from './components/WelcomeSteps';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-        <Navbar></Navbar>
+      <img src={logo} className="App-logo" alt="logo"/>
+      <WelcomeSteps />
+      <span></span>
     </div>
   );
 }
