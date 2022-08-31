fa293da1ca5746ed92e9bd63f15e507f02d36fe8
Hawke008 <82276201+Hawke008@users.noreply.github.com>
2022-08-31 12:34:10 +0200

    Quiz fonctionnel manque le changement de question

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './css/reset.css'
 import './css/helpers.css'
 import './App.css';
@@ -7,13 +6,13 @@ import WelcomeSteps from './components/WelcomeSteps';
 import Navbar from './components/Navbar';
 import logo from './assets/big_politips.png';
 import Category from './components/Category';
-import Kouizh from './components/Kouizh';
+import Quiz from './components/Quiz';
 
 function App() {
   return (
     <div className="App">
       <div className ='container'>
-        <Kouizh></Kouizh>
+        <Quiz></Quiz>
       </div>
     </div>
   );
